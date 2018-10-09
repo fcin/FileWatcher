e7960a91ba9ba614105e11a829d04002d7dc85c3
fcin <filip@cinik.eu>
2018-10-09 19:33:27 +0200

    Solution files

---
+++ FileWatcher/FileWatcher/FwLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace FileWatcher
+{
+    public sealed class FwLogger
+    {
+        public static void Error(string message, Exception exception, 
+            [CallerMemberName] string methodName = "", [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath ="")
+        {
+            Trace.WriteLine($"{DateTime.UtcNow} ERROR[at {methodName}:{lineNumber} in {Path.GetFileName(filePath)}]: {message}");
+            Trace.Indent();
+            Trace.WriteLine(exception.Message);
+            Trace.WriteLine(exception.StackTrace);
+            Trace.Unindent();
+        }
+
+        public static void Message(string message)
+        {
+            Trace.WriteLine($"{DateTime.UtcNow}: {message}");
+        }
+    }
+}
---
+++ FileWatcher/FileWatcher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            if (Environment.UserInteractive)
+            {
+
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new WatcherService()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
